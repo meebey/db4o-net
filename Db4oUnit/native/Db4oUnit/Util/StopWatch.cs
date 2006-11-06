f1185ff7ae479766fd2678ddc4d5d9e6f455c108
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-06 20:12:25 +0000

    more meaningful output for db4ounit tests
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@4395 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4oUnit/native/Db4oUnit/Util/StopWatch.cs
@@ -0,0 +1,33 @@
+namespace Db4oUnit.Util
+{
+	public class StopWatch
+	{
+		private System.DateTime _started;
+
+		private System.DateTime _finished;
+
+		public StopWatch()
+		{
+		}
+
+		public virtual void Start()
+		{
+			_started = System.DateTime.Now;
+		}
+
+		public virtual void Stop()
+		{
+			_finished = System.DateTime.Now;
+		}
+
+		public virtual System.TimeSpan Elapsed()
+		{
+			return _finished - _started;
+		}
+
+		public override string ToString()
+		{
+			return Elapsed().ToString();
+		}
+	}
+}
