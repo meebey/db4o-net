9422e7842d6d72260f87ab534bb8992fa3eccde6
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-09 19:42:04 +0000

    use native .net File operations whenever possible
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@4467 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Foundation/IO/File4.cs
@@ -0,0 +1,21 @@
+﻿/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+using System.IO;
+
+namespace Db4objects.Db4o.Foundation.IO
+{
+    public class File4
+    {
+        public static void Delete(string file)
+        {
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+
+        public static void Copy(string from, string to)
+        {
+            File.Copy(from, to, true);
+        }
+    }
+}
