3d6e710a0914cf1322a5e870c682e2174518a54b
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-10 22:23:17 +0000

    cr: Batch file to help switch between Visual Studio versions.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@4493 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ delete_binaries.bat
@@ -0,0 +1,11 @@
+REM This batch helps to switch between VS.NET 2003 and VS.NET 2005
+del Db4objects.Db4o\bin /S /Q
+del Db4objects.Db4o\obj /S /Q
+del Db4objects.Db4o.Tests\bin /S /Q
+del Db4objects.Db4o.Tests\obj /S /Q
+del Db4objects.Db4o.Tools\bin /S /Q
+del Db4objects.Db4o.Tools\obj /S /Q
+del Db4oUnit\bin /S /Q
+del Db4oUnit\obj /S /Q
+del Db4oUnit.Extensions\bin /S /Q
+del Db4oUnit.Extensions\obj /S /Q
