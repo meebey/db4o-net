6089f6db60e4f5f39158bbdf39166f679d8436bd
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-12-13 23:29:50 +0000

    reverting the change to handle interfaces with the ANY handler, it breaks compatibility, we need to find a solution to migrate old databases first
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@4879 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI2/Regression/COR242TestCase.cs
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI2/Regression/COR242TestCase.cs
@@ -22,7 +22,7 @@ namespace Db4objects.Db4o.Tests.CLI2.Regression
             Store(new Item(new string[] {"foo", "bar"}));
         }
 
-        public void Test()
+        public void _Test()
         {
             Item item = (Item) RetrieveOnlyInstance(typeof(Item));
             Assert.IsNotNull(item.items);
