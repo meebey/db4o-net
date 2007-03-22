a840fcc18b11d43b553f3c829612ca6143a1c82a
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-22 17:02:37 +0000

    jbe+rbo: implement a specific and probably much more efficient SimpleTimer for .net.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@5823 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Foundation/SimpleTimer.cs
@@ -0,0 +1,29 @@
+﻿/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
+
+using System;
+using System.Threading;
+
+using Sharpen.Lang;
+
+namespace Db4objects.Db4o.Foundation
+{
+	public class SimpleTimer
+	{
+		private readonly Timer _timer;
+
+		public SimpleTimer(IRunnable runnable, int interval, string name)
+		{
+			_timer = new Timer(new TimerCallback(Run), runnable, 0, interval);
+		}
+
+		private static void Run(object state)
+		{
+			((IRunnable)state).Run();
+		}
+
+		public void Stop()
+		{
+			_timer.Dispose();
+		}
+	}
+}
