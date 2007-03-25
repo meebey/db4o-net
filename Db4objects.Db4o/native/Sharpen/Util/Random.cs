6fc0de523ba911f801a1240c4f221f24928bfd6f
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-25 12:48:32 +0000

    Ported database identity issue from 6.1
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@5874 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Sharpen/Util/Random.cs
+++ Db4objects.Db4o/native/Sharpen/Util/Random.cs
@@ -1,20 +1,20 @@
-/* Copyright (C) 2004 - 2006  db4objects Inc.  http://www.db4o.com */
+﻿/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
 
 using System.Collections;
 
-namespace Sharpen.Util {
-	public class Random {
+namespace Sharpen.Util 
+{
+	public class Random 
+	{
+		System.Random _random = new System.Random();
 
-		private long _lastValue;
-
-		public long NextLong() {
-			long newValue = Sharpen.Runtime.CurrentTimeMillis();
-			if(newValue <= _lastValue)
-			{
-				newValue = _lastValue +1;
-			}
-			_lastValue = newValue;
-			return _lastValue;
+		public Random() 
+		{
+		}
+		
+		public long NextLong() 
+		{
+			return _random.Next();
 		}
 	}
 }
