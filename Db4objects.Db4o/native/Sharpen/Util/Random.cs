db038c59ac0f1ef494f105c183c105997d20f53c
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-25 10:53:47 +0000

    az + cr: Random generator fixed to make sure it generates unique values in one session.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@5871 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Sharpen/Util/Random.cs
+++ Db4objects.Db4o/native/Sharpen/Util/Random.cs
@@ -1,14 +1,20 @@
-﻿/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2004 - 2006  db4objects Inc.  http://www.db4o.com */
 
 using System.Collections;
 
 namespace Sharpen.Util {
 	public class Random {
-		public Random() {
-		}
-		
+
+		private long _lastValue;
+
 		public long NextLong() {
-			return Sharpen.Runtime.CurrentTimeMillis();
+			long newValue = Sharpen.Runtime.CurrentTimeMillis();
+			if(newValue <= _lastValue)
+			{
+				newValue = _lastValue +1;
+			}
+			_lastValue = newValue;
+			return _lastValue;
 		}
 	}
 }
