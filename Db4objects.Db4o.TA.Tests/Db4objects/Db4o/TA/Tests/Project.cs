cbc05feda488379e4adb6a51744c83df630c6fb4
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-11 02:34:30 +0000

    sharpening for db4ota project
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@6125 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.TA.Tests/Db4objects/Db4o/TA/Tests/Project.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using Db4objects.Db4o;
+using Db4objects.Db4o.Collections;
+using Db4objects.Db4o.TA;
+using Db4objects.Db4o.TA.Internal;
+using Db4objects.Db4o.TA.Tests;
+
+namespace Db4objects.Db4o.TA.Tests
+{
+	internal class Project : IActivatable
+	{
+		internal IList _subProjects = new PagedList();
+
+		internal IList _workLog = new PagedList();
+
+		internal string _name;
+
+		[System.NonSerialized]
+		internal Activator _activator;
+
+		public Project(string name)
+		{
+			_name = name;
+		}
+
+		public virtual void Bind(IObjectContainer container)
+		{
+			if (null != _activator)
+			{
+				_activator.AssertCompatible(container);
+				return;
+			}
+			_activator = new Activator(container, this);
+		}
+
+		private void Activate()
+		{
+			if (_activator == null)
+			{
+				return;
+			}
+			_activator.Activate();
+		}
+
+		public virtual void LogWorkDone(UnitOfWork work)
+		{
+			_workLog.Add(work);
+		}
+
+		public virtual long TotalTimeSpent()
+		{
+			Activate();
+			long total = 0;
+			for (IEnumerator iter = _workLog.GetEnumerator(); iter.MoveNext(); )
+			{
+				UnitOfWork item = (UnitOfWork)iter.Current;
+				total += item.TimeSpent();
+			}
+			return total;
+		}
+	}
+}
---
+++ Db4objects.Db4o.TA/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Db4objects.Db4o.TA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Db4objects, Inc.")]
+[assembly: AssemblyProduct("Db4objects.Db4o.TA")]
+[assembly: AssemblyCopyright("Copyright © Db4objects, Inc. 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be81bfcf-69e9-4f49-a4a2-54358e948e22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
