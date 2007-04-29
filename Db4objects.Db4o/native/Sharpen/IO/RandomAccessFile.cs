0812a7d46d847e193501f7e98c932423d8c9b7e7
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-29 18:14:48 +0000

    az: #COR-456 Fixed .net build.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@6274 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Sharpen/IO/RandomAccessFile.cs
+++ Db4objects.Db4o/native/Sharpen/IO/RandomAccessFile.cs
@@ -1,8 +1,8 @@
-﻿/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+﻿/* Copyright (C) 2004 - 2007  db4objects Inc.   http://www.db4o.com */
 
 using System;
 using System.IO;
-using Db4objects.Db4o.Ext;
+using Db4objects.Db4o;
 
 namespace Sharpen.IO
 {
