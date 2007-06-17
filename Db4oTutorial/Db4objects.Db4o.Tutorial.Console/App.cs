5d7f5c43bcaeaa6c98fcdd756d4aeaa0427f16c6
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2007-06-17 13:45:15 +0000

    tl: removed unused sources
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@6747 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4oTutorial/Db4objects.Db4o.Tutorial.Console/App.cs
+++ Db4oTutorial/Db4objects.Db4o.Tutorial.Console/App.cs
@@ -13,11 +13,6 @@ namespace Db4objects.Db4o.Tutorial.Console
 			Db4objects.Db4o.Tutorial.F1.Chapter4.InheritanceExample.Main(args);
 			Db4objects.Db4o.Tutorial.F1.Chapter5.ClientServerExample.Main(args);
 			Db4objects.Db4o.Tutorial.F1.Chapter6.TranslatorExample.Main(args);
-			Db4objects.Db4o.Tutorial.F1.Chapter21.IndexedExample.fillUpDB();
-			Db4objects.Db4o.Tutorial.F1.Chapter21.IndexedExample.noIndex();
-			Db4objects.Db4o.Tutorial.F1.Chapter21.IndexedExample.fullIndex();
-			Db4objects.Db4o.Tutorial.F1.Chapter21.IndexedExample.pilotIndex();
-			Db4objects.Db4o.Tutorial.F1.Chapter21.IndexedExample.pointsIndex();
 		}
 	}
 }
