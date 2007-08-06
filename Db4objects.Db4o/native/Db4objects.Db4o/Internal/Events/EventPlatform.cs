9fef7c47ed090b1983efda98d3d053573993d68c
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-08-06 17:18:57 +0000

    COR-666: transaction-aware event args
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@7225 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Db4objects.Db4o/Internal/Events/EventPlatform.cs
+++ Db4objects.Db4o/native/Db4objects.Db4o/Internal/Events/EventPlatform.cs
@@ -2,6 +2,7 @@
 
 using Db4objects.Db4o.Events;
 using Db4objects.Db4o.Ext;
+using Db4objects.Db4o.Internal;
 
 namespace Db4objects.Db4o.Internal.Events
 {
@@ -13,27 +14,27 @@ namespace Db4objects.Db4o.Internal.Events
 			e(klass, new ClassEventArgs(klass));
 		}
 		
-		public static void TriggerQueryEvent(QueryEventHandler e, Db4objects.Db4o.Query.IQuery q)
+		public static void TriggerQueryEvent(Transaction transaction, QueryEventHandler e, Db4objects.Db4o.Query.IQuery q)
 		{
 			if (null == e) return;
-			e(q, new QueryEventArgs(q));
+			e(q, new QueryEventArgs(transaction, q));
 		}
 
-		public static bool TriggerCancellableObjectEventArgs(CancellableObjectEventHandler e, object o)
+		public static bool TriggerCancellableObjectEventArgs(Transaction transaction, CancellableObjectEventHandler e, object o)
 		{
 			if (null == e) return true;
-			CancellableObjectEventArgs coea = new CancellableObjectEventArgs(o);
+			CancellableObjectEventArgs coea = new CancellableObjectEventArgs(transaction, o);
 			e(o, coea);
 			return !coea.IsCancelled;
 		}
 
-		public static void TriggerObjectEvent(ObjectEventHandler e, object o)
+		public static void TriggerObjectEvent(Transaction transaction, ObjectEventHandler e, object o)
 		{
 			if (null == e) return;
-			e(o, new ObjectEventArgs(o));
+			e(o, new ObjectEventArgs(transaction, o));
 		}
 		
-		public static void TriggerCommitEvent(CommitEventHandler e, object transaction, CallbackObjectInfoCollections objectInfoCollections)
+		public static void TriggerCommitEvent(Transaction transaction, CommitEventHandler e, CallbackObjectInfoCollections objectInfoCollections)
 		{
 			if (null == e) return;
             e(null, new CommitEventArgs(transaction, objectInfoCollections));
