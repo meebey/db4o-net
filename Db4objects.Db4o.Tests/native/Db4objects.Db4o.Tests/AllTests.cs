e6d5d9aacdddaa156e8b2abb338dec005f9d184f
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-08-18 05:13:28 +0000

    rl: fix COR-733
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@7337 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/AllTests.cs
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/AllTests.cs
@@ -21,38 +21,7 @@ namespace Db4objects.Db4o.Tests
 		{
 			return new Type[]
 				{	
-#if CF_1_0 || CF_2_0
-                    typeof(Db4objects.Db4o.Tests.Common.Acid.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Assorted.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Btree.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Classindex.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Config.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Constraints.AllTests), 
-                    //typeof(Db4objects.Db4o.Tests.Common.CS.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Defragment.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Events.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Exceptions.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Ext.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Fatalerror.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Fieldindex.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Foundation.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Freespace.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Handlers.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Header.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Interfaces.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Internal.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.IO.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Reflect.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Regression.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Querying.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Set.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Soda.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Stored.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Common.Types.AllTests), 
-                    typeof(Db4objects.Db4o.Tests.Util.Test.AllTests),
-#else
                     typeof(Db4objects.Db4o.Tests.Common.AllTests),
-#endif
                     typeof(Db4objects.Db4o.Tests.CLI1.AllTests),
 					typeof(Db4objects.Db4o.Tests.CLI2.AllTests),
                     typeof(Db4objects.Db4o.Tests.SharpenLang.AllTests),
