8cc5ad3b3baf303aa2378c95df48b90c0b1b7806
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-02 12:44:59 +0000

    cr: Knowing whether we run on Java or .NET helps a lot.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@7782 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4oUnit/native/Db4oUnit/Util/PlatformInformation.cs
@@ -0,0 +1,16 @@
+/* Copyright (C) 2004 - 2007  db4objects Inc.  http://www.db4o.com */
+namespace Db4oUnit.Util
+{
+	public class PlatformInformation
+	{
+		public static bool IsJava()
+		{
+			return false;
+		}
+
+		public static bool IsDotNet()
+		{
+			return true;
+		}
+	}
+}
