6712f712d179d9a03ffa6f1507e714b5f84bb7b3
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-11-23 18:37:51 +0000

    cr: #COR-988 Db4oAdmin is now Db4oTool
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@8575 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4oAdmin/Db4oAdmin/Core/IAssemblyInstrumentation.cs
+++
@@ -1,8 +0,0 @@
-﻿/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
-namespace Db4oTool.Core
-{
-	public interface IAssemblyInstrumentation
-	{
-		void Run(InstrumentationContext context);
-	}
-}
--- Db4oAdmin/Db4oAdmin/Core/ITypeFilter.cs
+++
@@ -1,9 +0,0 @@
-using Mono.Cecil;
-
-namespace Db4oTool.Core
-{
-    public interface ITypeFilter
-    {
-        bool Accept(TypeDefinition typeDef);
-    }
-}
