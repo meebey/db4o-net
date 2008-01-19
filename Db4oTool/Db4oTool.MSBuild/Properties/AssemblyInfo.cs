22e528b8481357616b6ce8786fa82c8d4e4c534c
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2008-01-19 15:49:18 +0000

    rl: fix COR-1099
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@9087 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4oTool/Db4oTool.MSBuild/Properties/AssemblyInfo.cs
+++ Db4oTool/Db4oTool.MSBuild/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Db4oTool.MSBuild")]
 [assembly: AssemblyDescription("db4o MSBuild tasks")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("db4objects Inc.")]
-[assembly: AssemblyProduct("Db4oTool.MSBuild")]
-[assembly: AssemblyCopyright("db4objects Inc. 2007")]
+[assembly: AssemblyCompany("db4objects Inc., San Mateo, CA, USA")]
+[assembly: AssemblyProduct("Db4oTool.MSBuild - Db4oEnhancerMSBuildTask")]
+[assembly: AssemblyCopyright("db4o 2005 - 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,5 +17,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("c592b83f-7c81-4008-8271-7c8c133e3a8c")]
 
-[assembly: AssemblyVersion("7.0.0.1")]
-[assembly: AssemblyFileVersion("7.0.0.1")]
+[assembly: AssemblyVersion("7.1.26.9085")]
