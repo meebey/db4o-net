168ef6ff0b5ccb67c9e95d74edd8c91756101234
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-05 11:50:42 +0000

    rbo+acv: COR-1108 - changing to msbuild
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@9271 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4oTool/Db4oTool/Properties/AssemblyInfo.cs
+++ Db4oTool/Db4oTool/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Security;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("7.1.28.9219")]
+[assembly: AssemblyVersion("7.1.29.9219")]
 
 [assembly: Mono.Author("Jean Baptiste Evain")]
 [assembly: Mono.Author("Rodrigo B. de Oliveira")]
