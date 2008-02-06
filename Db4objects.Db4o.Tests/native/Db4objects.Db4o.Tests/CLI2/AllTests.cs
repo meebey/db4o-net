d3b9d9ffd9720ea732ded02dcdaa18a2f41bbbc7
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-06 20:42:07 +0000

    rbo+acv: somehow the file disappeared
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@9297 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI2/AllTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Db4oUnit.Extensions;
+
+namespace Db4objects.Db4o.Tests.CLI2
+{
+	class AllTests : Db4oTestSuite
+	{
+		protected override Type[] TestCases()
+		{
+			return new Type[]
+			{
+				typeof(Assorted.AllTests),
+				typeof(Collections.AllTests),
+				typeof(NativeQueries.Diagnostics.AllTests),
+				typeof(Regression.AllTests),
+				typeof(TA.AllTests),
+				typeof(Types.AllTests),
+			};
+		}
+	}
+}
