534fe6e22b01ee595edf4dbc3acf34c032700bc4
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-03-04 23:24:50 +0000

    test case should not depend on the global configuration
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10399 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Reflect/Net/UninitializedObjectsWithFinalizerTestCase.cs
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Reflect/Net/UninitializedObjectsWithFinalizerTestCase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Db4objects.Db4o.Config;
 using Db4oUnit;
 
 namespace Db4objects.Db4o.Tests.CLI1.Reflect.Net
@@ -8,39 +9,34 @@ namespace Db4objects.Db4o.Tests.CLI1.Reflect.Net
 	class UninitializedObjectsWithFinalizerTestCase : ITestCase
 	{
 #if !CF
-		private const string DatabaseFileName = "TestUnitializedObjects.odb";
 		public void TestUninitilizedObjects()
 		{
-			bool exceptionNotThrown = true;
-			try
+			string databaseFileName = Path.GetTempFileName();
+			File.Delete(databaseFileName);
+
+			using (IObjectContainer db = Db4oFactory.OpenFile(NewConfiguration(), databaseFileName))
 			{
-				File.Delete(DatabaseFileName);
-
-				using (IObjectContainer db = Db4oFactory.OpenFile(DatabaseFileName))
-				{
-					db.Store(new TestSubject("Test"));
-					
-					GC.Collect();
-					GC.WaitForPendingFinalizers();
-				}
-
-				using (IObjectContainer db = Db4oFactory.OpenFile(DatabaseFileName))
-				{
-					IList <TestSubject> result = db.Query<TestSubject>();
-					
-					Assert.AreEqual(1, result.Count);
-					db.Activate(result[0], 2);
-					Assert.AreEqual("Test", result[0].name);
-                }
-
-				File.Delete(DatabaseFileName);
-			}
-			catch
-			{
-				exceptionNotThrown = false;
+				db.Store(new TestSubject("Test"));
+				
+				GC.Collect();
+				GC.WaitForPendingFinalizers();
 			}
 
-			Assert.IsTrue(exceptionNotThrown);
+			using (IObjectContainer db = Db4oFactory.OpenFile(NewConfiguration(), databaseFileName))
+			{
+				IList <TestSubject> result = db.Query<TestSubject>();
+				
+				Assert.AreEqual(1, result.Count);
+				db.Activate(result[0], 2);
+				Assert.AreEqual("Test", result[0].name);
+            }
+
+			File.Delete(databaseFileName);
+		}
+
+		private static IConfiguration NewConfiguration()
+		{
+			return Db4oFactory.NewConfiguration();
 		}
 	}
 
