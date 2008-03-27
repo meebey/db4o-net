38591cfd1fdf47a34c9f910b06c4b0af1c8f9fb0
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-03-27 01:12:28 +0000

    MoveNext must be evaluated inside the db4o lock (deferred query evaluation mode)
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10446 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Db4objects.Db4o/Internal/Query/ObjectSetFacade.cs
+++ Db4objects.Db4o/native/Db4objects.Db4o/Internal/Query/ObjectSetFacade.cs
@@ -1,5 +1,6 @@
 /* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
 using System;
+using System.Collections;
 using Db4objects.Db4o.Ext;
 using Db4objects.Db4o.Internal.Query.Result;
 
@@ -20,7 +21,8 @@ namespace Db4objects.Db4o.Internal.Query
 
 		#region IObjectSet Members
 		
-		public Object Get(int index) {
+		public Object Get(int index)
+		{
             return _delegate.Get(index);
         }
 
@@ -182,48 +184,30 @@ namespace Db4objects.Db4o.Internal.Query
 		}
 
 		#endregion
-
-		#region IEnumerable Members
-
-		class ObjectSetImplEnumerator : System.Collections.IEnumerator
-		{
-			StatefulQueryResult _result;
-			int _next = 0;
-			
-			public ObjectSetImplEnumerator(StatefulQueryResult result)
-			{
-				_result = result;
-			}
-
-			public void Reset()
-			{
-				_next = 0;
-			}
-
-			public object Current
-			{
-				get
-				{
-					return _result.Get(_next-1);
-				}
-			}
-
-			public bool MoveNext()
-			{
-				if (_next < _result.Size())
-				{
-					++_next;
-					return true;
-				}
-				return false;
-			}
-		}
-
+		
 		public System.Collections.IEnumerator GetEnumerator()
 		{
-			return new ObjectSetImplEnumerator(_delegate);
+			IEnumerator enumerator = _delegate.GetEnumerator();
+			object current;
+			while (MoveNext(enumerator, out current))
+			{
+				yield return current;
+			}
 		}
-		#endregion
+
+		private bool MoveNext(IEnumerator enumerator, out object current)
+		{
+			lock (_delegate.Lock())
+			{
+				if (enumerator.MoveNext())
+				{
+					current = enumerator.Current;
+					return true;
+				}
+			}
+			current = null;
+			return false;
+		}
 	}
 }
 
