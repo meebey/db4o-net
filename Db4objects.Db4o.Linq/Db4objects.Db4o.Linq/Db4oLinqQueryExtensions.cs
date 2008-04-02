6bbcaa1cefe9facfe3230a48249f905c9e9107fd
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-02 18:45:48 +0000

    spaces to tabs
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10506 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Linq/Db4objects.Db4o.Linq/Db4oLinqQueryExtensions.cs
+++ Db4objects.Db4o.Linq/Db4objects.Db4o.Linq/Db4oLinqQueryExtensions.cs
@@ -10,96 +10,96 @@ using Db4objects.Db4o.Linq.Internals;
 
 namespace Db4objects.Db4o.Linq
 {
-    /// <summary>
-    /// A class that defines some standard linq query operations
-    /// that can be optimized.
-    /// </summary>
-    public static class Db4oLinqQueryExtensions
-    {
-        public static IDb4oLinqQuery<TSource> Where<TSource>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, bool>> expression)
-        {
-            return Process(self,
-                query => new WhereClauseVisitor().Process(expression),
-                data => data.UnoptimizedWhere(expression.Compile())
-            );
-        }
+	/// <summary>
+	/// A class that defines some standard linq query operations
+	/// that can be optimized.
+	/// </summary>
+	public static class Db4oLinqQueryExtensions
+	{
+		public static IDb4oLinqQuery<TSource> Where<TSource>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, bool>> expression)
+		{
+			return Process(self,
+				query => new WhereClauseVisitor().Process(expression),
+				data => data.UnoptimizedWhere(expression.Compile())
+			);
+		}
 
-        public static int Count<TSource>(this IDb4oLinqQuery<TSource> self)
-        {
-            if (self == null)
-                throw new ArgumentNullException("self");
+		public static int Count<TSource>(this IDb4oLinqQuery<TSource> self)
+		{
+			if (self == null)
+				throw new ArgumentNullException("self");
 
-            var query = self as Db4oQuery<TSource>;
-            if (query != null)
-                return query.Count;
+			var query = self as Db4oQuery<TSource>;
+			if (query != null)
+				return query.Count;
 
-            return Enumerable.Count(self);
-        }
+			return Enumerable.Count(self);
+		}
 
-        delegate IEnumerable<T> FallbackProcessor<T>(IDb4oLinqQueryInternal<T> query);
+		delegate IEnumerable<T> FallbackProcessor<T>(IDb4oLinqQueryInternal<T> query);
 
-        private static IDb4oLinqQuery<TSource> Process<TSource>(
-            IDb4oLinqQuery<TSource> query,
-            Func<Db4oQuery<TSource>, IQueryBuilderRecord> queryProcessor,
-            FallbackProcessor<TSource> fallbackProcessor)
-        {
-            if (query == null)
-                throw new ArgumentNullException("query");
+		private static IDb4oLinqQuery<TSource> Process<TSource>(
+			IDb4oLinqQuery<TSource> query,
+			Func<Db4oQuery<TSource>, IQueryBuilderRecord> queryProcessor,
+			FallbackProcessor<TSource> fallbackProcessor)
+		{
+			if (query == null)
+				throw new ArgumentNullException("query");
 
-            var candidate = query as Db4oQuery<TSource>;
+			var candidate = query as Db4oQuery<TSource>;
 
-            if (candidate == null)
-                return new UnoptimizedQuery<TSource>(fallbackProcessor((IDb4oLinqQueryInternal<TSource>)query));
+			if (candidate == null)
+				return new UnoptimizedQuery<TSource>(fallbackProcessor((IDb4oLinqQueryInternal<TSource>)query));
 
-            try
-            {
-                IQueryBuilderRecord record = queryProcessor(candidate);
-                return new Db4oQuery<TSource>(candidate, record);
-            }
-            catch (QueryOptimizationException)
-            {
-                return new UnoptimizedQuery<TSource>(fallbackProcessor(candidate));
-            }
-        }
+			try
+			{
+				IQueryBuilderRecord record = queryProcessor(candidate);
+				return new Db4oQuery<TSource>(candidate, record);
+			}
+			catch (QueryOptimizationException)
+			{
+				return new UnoptimizedQuery<TSource>(fallbackProcessor(candidate));
+			}
+		}
 
-        private static IDb4oLinqQuery<TSource> ProcessOrderBy<TSource, TKey>(IDb4oLinqQuery<TSource> query, OrderByClauseVisitorBase visitor, Expression<Func<TSource, TKey>> expression, FallbackProcessor<TSource> fallbackProcessor)
-        {
-            return Process(query, q => visitor.Process(expression), fallbackProcessor);
-        }
+		private static IDb4oLinqQuery<TSource> ProcessOrderBy<TSource, TKey>(IDb4oLinqQuery<TSource> query, OrderByClauseVisitorBase visitor, Expression<Func<TSource, TKey>> expression, FallbackProcessor<TSource> fallbackProcessor)
+		{
+			return Process(query, q => visitor.Process(expression), fallbackProcessor);
+		}
 
-        public static IDb4oLinqQuery<TSource> OrderBy<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
-        {
-            return ProcessOrderBy(self, new OrderByAscendingClauseVisitor(), expression,
-                data => data.OrderBy(expression.Compile())
-                );
-        }
+		public static IDb4oLinqQuery<TSource> OrderBy<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
+		{
+			return ProcessOrderBy(self, new OrderByAscendingClauseVisitor(), expression,
+				data => data.OrderBy(expression.Compile())
+			);
+		}
 
-        public static IDb4oLinqQuery<TSource> OrderByDescending<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
-        {
-            return ProcessOrderBy(self, new OrderByDescendingClauseVisitor(), expression,
-                data => data.OrderByDescending(expression.Compile())
-                );
-        }
+		public static IDb4oLinqQuery<TSource> OrderByDescending<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
+		{
+			return ProcessOrderBy(self, new OrderByDescendingClauseVisitor(), expression,
+				data => data.OrderByDescending(expression.Compile())
+			);
+		}
 
-        public static IDb4oLinqQuery<TSource> ThenBy<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
-        {
-            return ProcessOrderBy(self, new OrderByAscendingClauseVisitor(), expression,
-                data => data.UnoptimizedThenBy(expression.Compile())
-                );
-        }
+		public static IDb4oLinqQuery<TSource> ThenBy<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
+		{
+			return ProcessOrderBy(self, new OrderByAscendingClauseVisitor(), expression,
+				data => data.UnoptimizedThenBy(expression.Compile())
+			);
+		}
 
-        public static IDb4oLinqQuery<TSource> ThenByDescending<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
-        {
-            return ProcessOrderBy(self, new OrderByDescendingClauseVisitor(), expression,
-                data => data.UnoptimizedThenByDescending(expression.Compile())
-                );
-        }
+		public static IDb4oLinqQuery<TSource> ThenByDescending<TSource, TKey>(this IDb4oLinqQuery<TSource> self, Expression<Func<TSource, TKey>> expression)
+		{
+			return ProcessOrderBy(self, new OrderByDescendingClauseVisitor(), expression,
+				data => data.UnoptimizedThenByDescending(expression.Compile())
+			);
+		}
 
-        public static IDb4oLinqQuery<TRet> Select<TSource, TRet>(this IDb4oLinqQuery<TSource> self, Func<TSource, TRet> selector)
-        {
-            var temp = self as PlaceHolderQuery<TSource>;
-            if (temp == null) return new UnoptimizedQuery<TRet>(Enumerable.Select(self, selector));
-            return new Db4oQuery<TRet>(temp.Container);
-        }
-    }
+		public static IDb4oLinqQuery<TRet> Select<TSource, TRet>(this IDb4oLinqQuery<TSource> self, Func<TSource, TRet> selector)
+		{
+			var temp = self as PlaceHolderQuery<TSource>;
+			if (temp == null) return new UnoptimizedQuery<TRet>(Enumerable.Select(self, selector));
+			return new Db4oQuery<TRet>(temp.Container);
+		}
+	}
 }
