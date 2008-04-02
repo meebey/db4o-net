5d4b44af2d0739c4b0655409b16ebfb215ae23e9
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-02 18:31:27 +0000

    ng + acv: COR-1236 : Fix OrderBy bug when quering  instrumented (TA/TP) classes.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10504 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Linq/Db4objects.Db4o.Linq/Expressions/OrderByClauseVisitorBase.cs
@@ -0,0 +1,26 @@
+/* Copyright (C) 2007 - 2008  db4objects Inc.  http://www.db4o.com */
+
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Db4objects.Db4o.Linq.Caching;
+using Db4objects.Db4o.Linq.Internals;
+using Db4objects.Db4o.Query;
+
+namespace Db4objects.Db4o.Linq.Expressions
+{
+	internal abstract class OrderByClauseVisitorBase : ExpressionQueryBuilder
+	{
+        protected abstract void ApplyDirection(IQuery query);
+
+		protected override void VisitMemberAccess(MemberExpression m)
+		{
+			if (!IsParameterReference(m)) CannotOptimize(m);
+
+			ProcessMemberAccess(m);
+
+			Recorder.Add(ctx => ApplyDirection(ctx.CurrentQuery));
+		}
+	}
+}
---
+++ Db4objects.Db4o.Linq/Db4objects.Db4o.Linq/Expressions/OrderByDescendingClauseVisitor.cs
@@ -0,0 +1,28 @@
+/* Copyright (C) 2007 - 2008  db4objects Inc.  http://www.db4o.com */
+
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Db4objects.Db4o.Linq.Caching;
+using Db4objects.Db4o.Linq.Internals;
+using Db4objects.Db4o.Query;
+
+namespace Db4objects.Db4o.Linq.Expressions
+{
+    internal class OrderByDescendingClauseVisitor : OrderByClauseVisitorBase
+    {
+        private static ICachingStrategy<Expression, IQueryBuilderRecord> _cache =
+            new SingleItemCachingStrategy<Expression, IQueryBuilderRecord>(ExpressionEqualityComparer.Instance);
+
+        protected override ICachingStrategy<Expression, IQueryBuilderRecord> GetCachingStrategy()
+        {
+            return _cache;
+        }
+
+        protected override void ApplyDirection(IQuery query)
+        {
+            query.OrderDescending();
+        }
+    }
+}
