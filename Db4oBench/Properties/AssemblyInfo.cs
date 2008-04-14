72dede2f13c449a4f91a331171ac3055b3dea7ca
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-14 13:45:36 +0000

    acv+ng: DOC-299 - Db4oBench.dll version was incorrect.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10610 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4oBench/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Copyright (C) 2007 - 2008  db4objects Inc.  http://www.db4o.com */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Db4oBench")]
+[assembly: AssemblyDescription("Db4oBench 7.3.39.10608 (.NET)")]
+[assembly: AssemblyConfiguration(".NET")]
+[assembly: AssemblyCompany("db4objects Inc., San Mateo, CA, USA")]
+[assembly: AssemblyProduct("db4o - database for objects")]
+[assembly: AssemblyCopyright("db4o 2005 - 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible (false)]
+
+[assembly: AssemblyVersion("7.3.39.10608")]
