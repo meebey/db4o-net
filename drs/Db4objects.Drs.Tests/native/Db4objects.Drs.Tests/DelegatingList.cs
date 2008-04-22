b614ff59623da9c36dbe80e8129d32f65c167734
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-22 13:11:41 +0000

    rbo+pr: DRS-89 - allow custom collection interface implementations (collection subclasses are still not supported because db4o itself cant handle them).
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10661 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ drs/Db4objects.Drs.Tests/native/Db4objects.Drs.Tests/DelegatingList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+
+namespace Db4objects.Drs.Tests
+{
+	public class DelegatingList : IList
+	{
+		private IList _delegate;
+
+		public DelegatingList(IList @delegate)
+		{
+			_delegate = @delegate;
+		}
+
+		#region IList Members
+
+		public int Add(object value)
+		{
+			return _delegate.Add(value);
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int IndexOf(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(int index, object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsFixedSize
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool IsReadOnly
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void Remove(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveAt(int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object this[int index]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		#endregion
+
+		#region ICollection Members
+
+		public void CopyTo(Array array, int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Count
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool IsSynchronized
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public object SyncRoot
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		#endregion
+
+		#region IEnumerable Members
+
+		public IEnumerator GetEnumerator()
+		{
+			return _delegate.GetEnumerator();
+		}
+
+		#endregion
+	}
+}
--- drs/Db4objects.Drs/native/Db4objects.Drs/Inside/ReplicationPlatform.cs
+++ drs/Db4objects.Drs/native/Db4objects.Drs/Inside/ReplicationPlatform.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Db4objects.Db4o.Reflect;
+using Db4objects.Db4o.Reflect.Net;
 
 namespace Db4objects.Drs.Inside
 {
@@ -146,6 +147,39 @@ namespace Db4objects.Drs.Inside
 			if (null == o) return false;
 			return o.GetType().IsValueType;
 		}
-	}
+
+		static readonly Type[] _nonGeneric = new Type[] 
+        {
+            typeof(ArrayList),
+            typeof(SortedList),
+            typeof(Queue),
+            typeof(Stack),
+        };
+
+		static readonly Type[] _generic = new Type[] 
+		{
+			typeof(List<>),
+			typeof(LinkedList<>),
+			typeof(Stack<>),
+		};
+
+        internal static bool IsBuiltinCollectionClass(IReflector reflector, IReflectClass claxx)
+        {
+            Type type = NetReflector.ToNative(claxx);
+
+			if (Contains(_nonGeneric, type)) return true;
+
+			if (!type.IsGenericType) return false;
+
+			if (Contains(_generic, type.GetGenericTypeDefinition())) return true;
+
+			return false;
+        }
+
+		private static bool Contains(Type[] array, Type type)
+		{
+			return ((IList)array).Contains(type);
+		}
+    }
 }
 
