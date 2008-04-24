20a5a41d7bbe6f23bfcbbe91ac6114ed4a02ac9c
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-24 16:59:10 +0000

    rbo+pr:Iterator4Assert.sameContent
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10685 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4oUnit/native/Db4oUnit/ArrayAssert.cs
@@ -0,0 +1,10 @@
+﻿namespace Db4oUnit
+{
+	public partial class ArrayAssert
+	{
+		public static void AreEqual<T>(T[] expected, T[] actual)
+		{
+			Iterator4Assert.AreEqual(expected.GetEnumerator(), actual.GetEnumerator());
+		}
+	}
+}
