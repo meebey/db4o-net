41a1200235b680d798f77a938613fe655affd477
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-23 21:52:07 +0000

    DRS-97 - test case and fix
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@10845 915b37e7-0715-0410-8bc3-b14b59444072

--- drs/Db4objects.Drs.Tests/native/Db4objects.Drs.Tests/Regression/DelegateTestCase.cs
+++ drs/Db4objects.Drs.Tests/native/Db4objects.Drs.Tests/Regression/DelegateTestCase.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Db4objects.Db4o;
 using Db4oUnit;
 
 namespace Db4objects.Drs.Tests.Regression
@@ -11,7 +13,9 @@ namespace Db4objects.Drs.Tests.Regression
 
 			public string Value;
 
-			public Object untyped;
+			public object untyped;
+
+			public object[] array;
 
 			public Item(string value)
 			{
@@ -28,23 +32,79 @@ namespace Db4objects.Drs.Tests.Regression
 			}
 		}
 
-		public void Test()
+		public void TestDelegateFields()
 		{
 			Item item = new Item("the item");
 			item.Foo += delegate { };
-			item.untyped = new System.Action<string>(Console.WriteLine);
+			item.untyped = StringAction();
+			item.array = new object[] { StringAction() };
 
-			A().Provider().StoreNew(item);
-			A().Provider().Commit();
-
-			ReplicateAll(A().Provider(), B().Provider());
-
-			Item replicated = (Item)B().Provider().GetStoredObjects(typeof(Item))[0];
+			Item replicated = Replicate(item);
 			Assert.IsNotNull(replicated);
 			Assert.AreEqual(item.Value, replicated.Value);
 			Assert.AreEqual(0, replicated.HandlerCount);
 			Assert.IsNull(replicated.untyped);
-		
+			Assert.AreEqual(1, replicated.array.Length);
+			Assert.IsNull(replicated.array[0]);
+		}
+
+		private static Action<string> StringAction()
+		{
+			return new System.Action<string>(Console.WriteLine);
+		}
+
+		public class DictionaryHolder
+		{
+			public Dictionary<string, Action<string>> actionDictionary;
+			public object untyped;
+			public object[] array;
+		}
+
+		public void TestDictionaryHolder()
+		{
+			DictionaryHolder item = new DictionaryHolder();
+			item.actionDictionary = NewActionDictionary();
+			item.untyped = NewActionDictionary();
+			item.array = new object[] { NewActionDictionary() };
+
+			DictionaryHolder replicated = Replicate(item);
+			Assert.IsNotNull(replicated.actionDictionary);
+			Assert.IsNotNull(replicated.untyped);
+			Assert.IsNotNull(replicated.array[0]);
+		}
+
+		private static Dictionary<string, Action<string>> NewActionDictionary()
+		{
+			Dictionary<string, Action<string>> d = new Dictionary<string, Action<string>>();
+			d["print"] = StringAction();
+			return d;
+		}
+
+		private T Replicate<T>(T item)
+		{
+			StoreToA(item);
+
+			ReplicateAll();
+
+			return QueryReplicated<T>();
+		}
+
+		private T QueryReplicated<T>()
+		{
+			IObjectSet found = B().Provider().GetStoredObjects(typeof(T));
+			Assert.AreEqual(1, found.Count);
+			return (T)found.Next();
+		}
+
+		private void ReplicateAll()
+		{
+			ReplicateAll(A().Provider(), B().Provider());
+		}
+
+		private void StoreToA(object item)
+		{
+			A().Provider().StoreNew(item);
+			A().Provider().Commit();
 		}
 	}
 }
