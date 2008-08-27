f6bf669b27f6302f3fa3aaa5b24284b6f63faa3e
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-08-27 21:39:52 +0000

    acv + pr : COR-1372 - Trying to attach to delete()/deleting() events at client side throws an exception
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@11501 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Internal/Events/ClientEventRegistryImpl.cs
@@ -0,0 +1,36 @@
+﻿/* Copyright (C) 2008   db4objects Inc.   http://www.db4o.com */
+
+using System;
+using Db4objects.Db4o.Events;
+
+namespace Db4objects.Db4o.Internal.Events
+{
+	partial class ClientEventRegistryImpl
+	{
+		public override event ObjectEventHandler Deleted
+		{
+			add
+			{
+				throw new ArgumentException("delete() event is raised only at server side.");
+			}
+
+			remove
+			{
+				throw new ArgumentException("delete() event is raised only at server side.");
+			}
+		}
+
+		public override event CancellableObjectEventHandler Deleting
+		{
+			add
+			{
+				throw new ArgumentException("deleting() event is raised only at server side.");
+			}
+
+			remove
+			{
+				throw new ArgumentException("deleting() event is raised only at server side.");
+			}
+		}
+	}
+}
