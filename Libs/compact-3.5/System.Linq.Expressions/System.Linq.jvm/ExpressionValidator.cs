7698bab6145c675b1588024409ffca96bf26ca48
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2008-09-03 13:40:09 +0000

    cleanup
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@11562 915b37e7-0715-0410-8bc3-b14b59444072

--- Libs/compact-3.5/System.Linq.Expressions/System.Linq.jvm/ExpressionValidator.cs
+++ Libs/compact-3.5/System.Linq.Expressions/System.Linq.jvm/ExpressionValidator.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Reflection;
-using System.Collections.Specialized;
 using System.Linq.Expressions;
 
 namespace System.Linq.jvm {
