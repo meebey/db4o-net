2f95aa95b9cc941dc83cd7446c6d9fc171d077c8
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-09-11 17:07:13 +0000

    cr: #INT-45 UTF-8 String encoding support completed.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@11616 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Foundation/UTF8Encoder.cs
@@ -0,0 +1,21 @@
+﻿/* Copyright (C) 2008   db4objects Inc.   http://www.db4o.com */
+using System;
+using System.Text;
+
+namespace Db4objects.Db4o.Foundation
+{
+    public class UTF8Encoder
+    {
+        public byte[] Encode(String str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        public String Decode(byte[] bytes, int start, int length)
+        {
+            return Encoding.UTF8.GetString(bytes, start, length);
+        }
+
+    }
+    
+}
