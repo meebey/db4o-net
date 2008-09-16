ff480ced5eeca0e8b8207c2bd477a23b6b2d8504
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-09-16 10:25:27 +0000

    cr: #INT-45 String encoding interface now takes a StringEncoding as a parameter. Own StringEncoding classes can be written.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@11662 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Config/Encoding/UTF8StringEncoding.cs
@@ -0,0 +1,20 @@
+﻿/* Copyright (C) 2008   db4objects Inc.   http://www.db4o.com */
+using System;
+
+namespace Db4objects.Db4o.Config.Encoding
+{
+    public class UTF8StringEncoding : BuiltInStringEncoding
+    {
+        public override byte[] Encode(String str)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(str);
+        }
+
+        public override String Decode(byte[] bytes, int start, int length)
+        {
+            return System.Text.Encoding.UTF8.GetString(bytes, start, length);
+        }
+
+    }
+    
+}
