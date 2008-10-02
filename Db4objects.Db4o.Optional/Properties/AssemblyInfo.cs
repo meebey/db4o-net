2cae5b1a7a2723c55a18b887e713f4360d889850
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-02 16:27:45 +0000

    acv+rbo: COR-1393 - Missing folder
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@11829 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Optional/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright (C) 2004 - 2007   db4objects Inc.   http://www.db4o.com */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+[assembly: AssemblyTitle("db4o - optional functionality")]
+[assembly: AssemblyCompany("db4objects Inc., San Mateo, CA, USA")]
+[assembly: AssemblyProduct("db4o - database for objects")]
+[assembly: AssemblyCopyright("db4o 2005 - 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// attributes are automatically set by the build
+[assembly: AssemblyVersion("7.7.63.11804")]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyConfiguration(".NET")]
+[assembly: AssemblyDescription("Db4objects.Db4o.Optional 7.7.63.11804 (.NET)")]
+
+#if !CF
+[assembly: AllowPartiallyTrustedCallers]
+#endif
