c4ce8c19a4f5750ce96a41716a2a8601f0cd3ebb
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-21 11:52:11 +0000

    helper for quick performance measurements
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@11975 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Foundation/PerformanceTracer.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Db4objects.Db4o.Foundation
+{
+	public class PerformanceTracer
+	{
+		public delegate void Block();
+
+		public static long TraceTime(string label, Block block)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			block();
+			sw.Stop();
+			Trace.WriteLine(label + ": " + sw.ElapsedMilliseconds + "ms\n");
+			return sw.ElapsedMilliseconds;
+		}
+
+	}
+}
