56fcefa7e169102521a55ea48d0b648b0e965199
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-11-17 21:41:21 +0000

    Map.remove returns the removed object
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@12123 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Sharpen/Util/Collections.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+
+namespace Sharpen.Util
+{
+	public class Collections
+	{
+		public static object Remove(IDictionary dictionary, object key)
+		{
+			object removed = dictionary[key];
+			dictionary.Remove(key);
+			return removed;
+		}
+	}
+}
