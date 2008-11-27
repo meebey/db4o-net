2d4779bff918b4986d8f7034e375914a77372c7c
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-11-27 13:14:40 +0000

    acv: COR-1456 - Db4o now will throw an exception (instead of silently ignoring) reentrant calls from callbacks.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@12215 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Db4objects.Db4o/Internal/Events/EventPlatform.cs
+++ Db4objects.Db4o/native/Db4objects.Db4o/Internal/Events/EventPlatform.cs
@@ -2,53 +2,69 @@
 
 using Db4objects.Db4o.Events;
 using Db4objects.Db4o.Ext;
-using Db4objects.Db4o.Internal;
+using Db4objects.Db4o.Query;
+using Sharpen.Lang;
 
 namespace Db4objects.Db4o.Internal.Events
 {
 	internal class EventPlatform
 	{
-		public static void TriggerClassEvent(ClassEventHandler e, Db4objects.Db4o.Internal.ClassMetadata klass)
+		public static void TriggerClassEvent(ClassEventHandler e, ClassMetadata klass)
 		{
-			if (null == e) return;
-			e(klass, new ClassEventArgs(klass));
+			Trigger(delegate
+			{
+				if (null == e) return;
+				e(klass, new ClassEventArgs(klass));
+			});
 		}
-		
-		public static void TriggerQueryEvent(Transaction transaction, QueryEventHandler e, Db4objects.Db4o.Query.IQuery q)
+
+		public static void TriggerQueryEvent(Transaction transaction, QueryEventHandler e, IQuery q)
 		{
-			if (null == e) return;
-			e(q, new QueryEventArgs(transaction, q));
+			Trigger(delegate
+			{
+				if (null == e) return;
+			    e(q, new QueryEventArgs(transaction, q));
+			});
 		}
 
 		public static bool TriggerCancellableObjectEventArgs(Transaction transaction, CancellableObjectEventHandler e, object o)
 		{
-			if (null == e) return true;
-			CancellableObjectEventArgs coea = new CancellableObjectEventArgs(transaction, o);
-            try
-            {
-                e(o, coea);
-            }
-            catch (Db4oException)
-            {
-                throw;
-            }
-            catch (System.Exception exception)
-            {
-                throw new EventException(exception);
-            }
-			return !coea.IsCancelled;
+			bool ret = false;
+			Trigger(delegate
+			{
+				if (null == e)
+			    {
+					ret = true;
+			        return;
+				}
+				
+				CancellableObjectEventArgs coea = new CancellableObjectEventArgs(transaction, o);
+				
+				e(o, coea);
+				ret = !coea.IsCancelled;
+			});
+
+			return ret;
 		}
 
 		public static void TriggerObjectEvent(Transaction transaction, ObjectEventHandler e, object o)
 		{
-			if (null == e) return;
-			e(o, new ObjectEventArgs(transaction, o));
+			Trigger(delegate
+			{
+				if (null == e) return;
+				
+				e(o, new ObjectEventArgs(transaction, o));
+			});
 		}
 		
 		public static void TriggerCommitEvent(Transaction transaction, CommitEventHandler e, CallbackObjectInfoCollections objectInfoCollections)
 		{
-			if (null == e) return;
-            e(null, new CommitEventArgs(transaction, objectInfoCollections));
+			Trigger(delegate
+			{
+				if (null == e) return;
+				e(null, new CommitEventArgs(transaction, objectInfoCollections));
+			});
+		
 		}
 		
 		public static bool HasListeners(System.Delegate e)
@@ -58,8 +74,44 @@ namespace Db4objects.Db4o.Internal.Events
 
 		public static void TriggerObjectContainerEvent(IObjectContainer container, ObjectContainerEventHandler e)
 		{
-			if (null == e) return;
-			e(container, new ObjectContainerEventArgs(container));
+			Trigger(delegate
+			{
+				if (null == e) return;
+				e(container, new ObjectContainerEventArgs(container));
+			});
+		}
+
+		private static void Trigger(RunnableDelegate  runnable)
+		{
+            try
+            {
+                ObjectReference._inCallback.With(true, new DelegateRunnable(runnable));
+            }
+            catch (Db4oException)
+            {
+                throw;
+            }
+            catch (System.Exception exception)
+            {
+                throw new EventException(exception);
+            }
 		}
 	}
+
+	internal class DelegateRunnable : IRunnable
+	{
+		public DelegateRunnable(RunnableDelegate block)
+		{
+			_delegate = block;
+		}
+
+		public void Run()
+		{
+			_delegate();
+		}
+
+		private readonly RunnableDelegate _delegate;
+	}
+
+	internal delegate void RunnableDelegate();
 }
