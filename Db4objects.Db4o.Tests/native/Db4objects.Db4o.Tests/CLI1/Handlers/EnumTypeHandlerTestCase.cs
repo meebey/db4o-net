94f9509151880b8d8531b930745998d1ae2d2d08
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-12-16 18:09:30 +0000

    cr + pr: #COR-1045 A start into writing an enum Typehandler.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@12300 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Handlers/EnumTypeHandlerTestCase.cs
@@ -0,0 +1,231 @@
+﻿/* Copyright (C) 2004 - 2008  db4objects Inc.  http://www.db4o.com */
+
+using System;
+using System.Collections.Generic;
+using Db4objects.Db4o.Config;
+using Db4objects.Db4o.Typehandlers;
+using Db4objects.Db4o.Query;
+using Db4oUnit;
+using Db4oUnit.Extensions;
+
+namespace Db4objects.Db4o.Tests.CLI1.Handlers
+{
+
+    class EnumTypeHandlerTestCase : AbstractDb4oTestCase
+    {
+        enum EnumAsByte : byte
+        {
+            First,
+            Second,
+            Third
+        }
+
+        enum EnumAsInteger
+        {
+            First = -42,
+            Second,
+        }
+
+        enum EnumAsUInt : uint
+        {
+            First = 42,
+            Second,
+        }
+
+        enum EnumAsLong : long
+        {
+            First = -42,
+            Second = 37,
+            Third
+        }
+
+        enum EnumAsULong : ulong
+        {
+            First = 42,
+            Second = 37
+        }
+
+        [Flags]
+        enum ByteFlags : byte
+        {
+            First = 0x01,
+            Second = 0x02,
+            Third = 0x04
+        }
+
+        enum EnumAsSByte : sbyte
+        {
+            First = 2,
+            Second
+        }
+
+        class Item
+        {
+            public readonly EnumAsByte _asByte;
+            public readonly EnumAsInteger _asInteger;
+            public readonly EnumAsLong _asLong;
+
+            public Item(EnumAsByte asByte, EnumAsInteger asInteger, EnumAsLong asLong)
+            {
+                _asByte = asByte;
+                _asInteger = asInteger;
+                _asLong = asLong;
+            }
+
+            public override bool Equals(object obj)
+            {
+                Item rhs = (Item)obj;
+                if (rhs == null) return false;
+
+                if (rhs.GetType() != GetType()) return false;
+
+                return _asByte == rhs._asByte && _asInteger == rhs._asInteger && _asLong == rhs._asLong;
+            }
+
+            public override string ToString()
+            {
+                return _asByte + "/" + _asInteger + "/" + _asLong;
+            }
+        }
+
+        private static readonly Item[] _items = new Item[]
+		                      	{
+									new Item(EnumAsByte.First, EnumAsInteger.Second, EnumAsLong.Third),
+									new Item(EnumAsByte.Second, EnumAsInteger.Second, EnumAsLong.Second),
+									new Item(EnumAsByte.Third, EnumAsInteger.Second, EnumAsLong.First),
+									new Item((EnumAsByte)99, (EnumAsInteger) 98, (EnumAsLong) 97),
+		                      	};
+
+
+        protected override void Configure(IConfiguration config)
+        {
+            base.Configure(config);
+            config.RegisterTypeHandler(new EnumTypeHandlerPredicate(), new EnumTypeHandler());
+        }
+
+        protected override void Store()
+        {
+            foreach (Item item in _items)
+            {
+                Store(item);
+            }
+        }
+
+        public void _TestNativeQuery()
+        {
+            AssertItem(EnumAsByte.Second, AsByteFinder(), AsByteSelectorFor);
+            AssertItem(EnumAsByte.Third, AsByteFinder(), AsByteSelectorFor);
+        }
+
+        public void TestSODAQuery()
+        {
+            AssertAsByte();
+            AssertAsLong();
+        }
+
+        public void _TestInvalidEnumValue()
+        {
+            AssertItem((EnumAsByte)99, AsByteFinder());
+        }
+
+        public void TestRetrieveAll()
+        {
+            IQuery query = NewQuery(typeof(Item));
+            IObjectSet result = query.Execute();
+            Assert.AreEqual(_items.Length, result.Count);
+
+            Iterator4Assert.SameContent(result.GetEnumerator(), _items.GetEnumerator());
+        }
+
+        private void AssertAsLong()
+        {
+            Func<object, Item> itemFinder = delegate(object value)
+            {
+                return Find(_items,
+                                    delegate(Item candidate)
+                                    {
+                                        return candidate._asLong == (EnumAsLong)value;
+                                    });
+            };
+
+            AssertItem(EnumAsLong.First, itemFinder);
+            AssertItem(EnumAsLong.Second, itemFinder);
+        }
+
+        private void AssertAsByte()
+        {
+            AssertItem(EnumAsByte.Second, AsByteFinder());
+            AssertItem(EnumAsByte.Third, AsByteFinder());
+        }
+
+        private void AssertItem<T>(T expectedEnumValue, Func<object, Item> itemFinder)
+        {
+            IQuery query = NewQuery(typeof(Item));
+            query.Descend(FieldNameFor(typeof(T))).Constrain(expectedEnumValue);
+
+            IObjectSet result = query.Execute();
+
+            Assert.AreEqual(1, result.Count);
+            Item actual = (Item)result[0];
+            Assert.IsNotNull(actual);
+
+            Item expected = itemFinder(expectedEnumValue);
+            Assert.IsNotNull(expected);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private void AssertItem<T>(T expectedEnumValue, Func<object, Item> itemFinder, Func<T, Predicate<Item>> selectorBuilder)
+        {
+            IList<Item> items = Db().Query(selectorBuilder(expectedEnumValue));
+            Assert.AreEqual(1, items.Count);
+            Assert.IsNotNull(items[0]);
+
+            Item expected = itemFinder(expectedEnumValue);
+            Assert.IsNotNull(expected);
+
+            Assert.AreEqual(expected, items[0]);
+        }
+
+        private static string FieldNameFor(Type type)
+        {
+            return type.Name.Replace("EnumA", "_a");
+        }
+
+        static Predicate<Item> AsByteSelectorFor(EnumAsByte value)
+        {
+            return delegate(Item candidate)
+            {
+                return candidate._asByte == value;
+            };
+
+        }
+
+        private static Func<object, Item> AsByteFinder()
+        {
+            return delegate(object value)
+            {
+                return Find(_items, delegate(Item candidate)
+                    {
+                        return candidate._asByte == (EnumAsByte)value;
+                    });
+            };
+        }
+
+        private static T Find<T>(T[] array, System.Predicate<T> expected)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (expected(array[i]))
+                {
+                    return array[i];
+                }
+            }
+            return default(T);
+        }
+
+        // for .NET 2.0 compatibility
+        public delegate R Func<T, R>(T value);
+
+    }
+}
---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Typehandlers/EnumTypeHandler.cs
@@ -0,0 +1,127 @@
+﻿/* Copyright (C) 2004 - 2008  db4objects Inc.  http://www.db4o.com */
+
+using System;
+using Db4objects.Db4o.Foundation;
+using Db4objects.Db4o.Internal;
+using Db4objects.Db4o.Internal.Activation;
+using Db4objects.Db4o.Internal.Delete;
+using Db4objects.Db4o.Internal.Marshall;
+using Db4objects.Db4o.Marshall;
+using Db4objects.Db4o.Reflect;
+using Db4objects.Db4o.Reflect.Net;
+using Db4objects.Db4o.Typehandlers;
+
+namespace Db4objects.Db4o.Typehandlers
+{
+    public class EnumTypeHandler : IEmbeddedTypeHandler, ITypeFamilyTypeHandler
+    {
+        private class PreparedEnumComparison : IPreparedComparison
+        {
+            private readonly long _enumValue;
+            public PreparedEnumComparison(object obj)
+            {
+                if (obj is TransactionContext)
+                {
+                    obj = ((TransactionContext)obj)._object;
+                }
+
+                if (obj == null) return;
+                _enumValue = Convert.ToInt64(obj);
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (obj is TransactionContext)
+                {
+                    obj = ((TransactionContext)obj)._object;
+                }
+
+                if (obj == null) return 1;
+
+                long other = Convert.ToInt64(obj);
+                if (_enumValue == other) return 0;
+                if (_enumValue < other) return -1;
+
+                return 1;
+            }
+        }
+
+        public IPreparedComparison PrepareComparison(IContext context, object obj)
+        {
+            return new PreparedEnumComparison(obj);
+        }
+
+        public void Delete(IDeleteContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Defragment(IDefragmentContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Read(IReadContext context)
+        {
+            int classId = context.ReadInt();
+            ClassMetadata clazz = Container(context).ClassMetadataForId(classId);
+
+            Type enumType = NetReflector.ToNative(clazz.ClassReflector());
+            long enumValue = context.ReadLong();
+            return Enum.ToObject(enumType, enumValue); ;
+        }
+
+        public void Write(IWriteContext context, object obj)
+        {
+            int classId = ClassMetadataIdFor(context, obj);
+
+            context.WriteInt(classId);
+            context.WriteLong(Convert.ToInt64(obj));
+        }
+
+        public bool CanHold(IReflectClass claxx)
+        {
+            return NetReflector.ToNative(claxx).IsEnum;
+        }
+
+        public bool IsSimple()
+        {
+            return true;
+        }
+
+        public int LinkLength()
+        {
+            return Const4.IdLength + Const4.LongLength;
+        }
+
+        private static int ClassMetadataIdFor(IContext context, object obj)
+        {
+            IReflectClass claxx = Container(context).Reflector().ForObject(obj);
+            ClassMetadata clazz = Container(context).ProduceClassMetadata(claxx);
+
+            //TODO: Handle clazz == null!! Must not happen!
+
+            return clazz.GetID();
+        }
+
+        private static ITypeHandler4 StringTypeHandler(IContext context)
+        {
+            return Container(context).Handlers().TypeHandlerForClass(Container(context).Ext().Reflector().ForClass(typeof(string)));
+        }
+
+        private static ObjectContainerBase Container(IContext context)
+        {
+            return ((IInternalObjectContainer)context.ObjectContainer()).Container();
+        }
+
+    }
+
+    public class EnumTypeHandlerPredicate : ITypeHandlerPredicate
+    {
+        public bool Match(IReflectClass classReflector)
+        {
+            Type type = NetReflector.ToNative(classReflector);
+            return type.IsEnum;
+        }
+    }
+}
