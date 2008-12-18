11452b5ddb8d9731e368082b1c8721626565f7f9
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-12-18 15:53:18 +0000

    Introducing EnvironmentProvider for test suites that require a simple convention based environment
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@12308 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o/native/Db4objects.Db4o/Foundation/Environments.cs
@@ -0,0 +1,12 @@
+﻿namespace Db4objects.Db4o.Foundation
+{
+	public partial class Environments
+	{
+		public static string DefaultImplementationFor(System.Type type)
+		{
+			string ns = type.Namespace;
+			int lastDot = ns.LastIndexOf('.');
+			return ns.Substring(0, lastDot) + ".Internal." + ns.Substring(lastDot + 1) + "." + type.Name.Substring(1) + "Impl";
+		}
+	}
+}
