f1062c571138d1a5fecf40ae6084613c5dfdd689
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-06 14:23:43 +0000

    cr + pr: #COR-1533 Bug: Enum PeekPersisted returns duplicate enum
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@12568 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Staging/EnumTestCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Db4oUnit;
+using Db4oUnit.Extensions;
+
+namespace Db4objects.Db4o.Tests.CLI1
+{
+    public class EnumTestCase : AbstractDb4oTestCase
+    {
+
+        enum MyEnum { A, B, C, D, F, INCOMPLETE }; 
+
+        class Item { 
+
+            public MyEnum _enum; 
+
+         } 
+
+       protected override void Store()
+       {
+           Item item = new Item();
+           item._enum = MyEnum.C;
+           Store(item);
+       }
+
+       public void TestPeekPersisted()
+       {
+           Item item = (Item) RetrieveOnlyInstance(typeof (Item));
+           Item peeked = (Item) Db().PeekPersisted(item, int.MaxValue, true);
+           Assert.AreSame(item._enum, peeked._enum);
+       }
+    } 
+
+}
