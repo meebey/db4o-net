24ae6c2f78dffc1682ff8ab36f1a8f3147ff00df
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-20 13:40:43 +0000

    migrated to db4ounit style
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@12642 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/CsStructs.cs
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/CsStructs.cs
@@ -12,38 +12,48 @@ namespace Db4objects.Db4o.Tests.CLI1
 	{
 		public static string GUID = "6a0d8033-444e-4b44-b0df-bf33dfe050f9";
 
-		SimpleStruct simpleStruct;
-		RecursiveStruct recursiveStruct;
-		Guid guid;
-
-		public CsStructs()
+		public class Item
 		{
+			public SimpleStruct simpleStruct;
+			public RecursiveStruct recursiveStruct;
+			public Guid guid;
+		}
+
+		public struct SimpleStruct
+		{
+			public int foo;
+			public string bar;
+		}
+
+		public struct RecursiveStruct
+		{
+			public Item child;
 		}
 
 		protected override void  Store()
 	    {
-			simpleStruct.foo = 100;
-			simpleStruct.bar = "hi";
+			Item item = new Item();
+			item.simpleStruct.foo = 100;
+			item.simpleStruct.bar = "hi";
 
 			RecursiveStruct r = new RecursiveStruct();
-			r.child = new CsStructs();
+			r.child = new Item();
 
 			SimpleStruct s = new SimpleStruct();
 			s.foo = 22;
 			s.bar = "jo";
 			r.child.simpleStruct = s;
 
-			recursiveStruct = r;
+			item.recursiveStruct = r;
 
-			guid = new Guid(GUID);
+			item.guid = new Guid(GUID);
 		    
-		    Store(this);
+		    Store(item);
 		}
 
 		public void Test()
 		{
-			IExtObjectContainer oc = Db();
-            IQuery q = NewQuery(GetType());
+			IQuery q = NewQuery(typeof(Item));
 			IQuery qd = q.Descend("simpleStruct");
 			qd = qd.Descend("foo");
 			qd.Constrain(100);
@@ -51,7 +61,7 @@ namespace Db4objects.Db4o.Tests.CLI1
 			IObjectSet objectSet = q.Execute();
 
 			Assert.AreEqual(1, objectSet.Count);
-			CsStructs csStructs = (CsStructs)objectSet.Next();
+			Item csStructs = (Item)objectSet.Next();
 
 			Assert.AreEqual(GUID, csStructs.guid.ToString());
             Assert.AreEqual(100, csStructs.simpleStruct.foo);
@@ -60,16 +70,5 @@ namespace Db4objects.Db4o.Tests.CLI1
             Assert.AreEqual("jo", csStructs.recursiveStruct.child.simpleStruct.bar);
 		}
 
-	}
-
-	public struct SimpleStruct
-	{
-		public int foo;
-		public string bar;
-	}
-
-	public struct RecursiveStruct
-	{
-		public CsStructs child;
 	}
 }
