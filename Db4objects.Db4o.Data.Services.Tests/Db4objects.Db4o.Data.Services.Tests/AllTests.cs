c8bf226b5c67cf293236851e204c72708ee6c21c
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2009-04-04 12:32:48 +0000

    add tests for the astoria provider
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@12935 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Data.Services.Tests/Db4objects.Db4o.Data.Services.Tests/AllTests.cs
@@ -0,0 +1,23 @@
+﻿/* Copyright (C) 2007 - 2008  db4objects Inc.  http://www.db4o.com */
+
+using System;
+using Db4oUnit.Extensions;
+
+namespace Db4objects.Db4o.Data.Services.Tests
+{
+	public class AllTests : Db4oTestSuite
+	{
+		public static int Main(string[] args)
+		{
+			var res = new AllTests().RunSolo();
+			return res;
+		}
+
+		protected override Type[] TestCases()
+		{
+			return new []{
+				typeof(Db4oDataContextTestCase),
+			};
+		}
+	}
+}
