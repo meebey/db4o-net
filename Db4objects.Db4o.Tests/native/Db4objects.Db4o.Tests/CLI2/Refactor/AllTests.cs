a276d9e758857f433f417f9c9ebcc9e67de74ad0
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-04-21 16:26:36 +0000

    pr+rbo: COR-1626 - use the correct null representation for value types when rebuilding indices
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13017 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI2/Refactor/AllTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Db4oUnit.Extensions;
+
+namespace Db4objects.Db4o.Tests.CLI2.Refactor
+{
+	class AllTests : Db4oTestSuite
+	{
+		#region Overrides of Db4oTestSuite
+
+		protected override Type[] TestCases()
+		{
+			return new Type[] { typeof(AddIndexedValueTypeFieldTestCase) };
+		}
+
+		#endregion
+	}
+}
