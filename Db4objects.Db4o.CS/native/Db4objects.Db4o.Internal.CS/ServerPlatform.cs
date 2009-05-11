8692719f093558a93e574cd830d38835d6e50a72
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-11 14:02:15 +0000

    cr/rbo - COR-1632 - initial work on batch activation - introduced ClientServerConfiguration#prefetchDepth to control batching behavior (currently it's all or nothing for depth 1)
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13098 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.CS/native/Db4objects.Db4o.Internal.CS/ServerPlatform.cs
@@ -0,0 +1,15 @@
+/* Copyright (C) 2009  Versant Inc.  http://www.db4o.com */
+
+namespace Db4objects.Db4o.Internal.CS
+{
+	class ServerPlatform
+	{
+		internal static void TriggerMessageEvent(MessageEventHandler eventHandler, Db4objects.Db4o.Internal.CS.Messages.Msg message)
+		{
+			if (eventHandler == null)
+				return;
+
+			eventHandler(null, new MessageEventArgs(message));
+		}
+	}
+}
