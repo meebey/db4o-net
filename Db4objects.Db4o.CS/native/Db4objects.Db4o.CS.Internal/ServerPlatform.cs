ea2c9251c2fa3404d0fbb7c63b8db405d0c829ad
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-26 14:31:22 +0000

    pr/rbo: COR-1632 - fix for the two failures uncovered by the legacy tests (GetAllSoda and TransientClone): ensure correct transaction is used in C/S mode during the delete callback and disable client slot cache during peekPersisted; Restructured db4o.cs namespaces: com.db4o.internal.cs => com.db4o.cs.internal
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13194 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.CS/native/Db4objects.Db4o.CS.Internal/ServerPlatform.cs
+++ Db4objects.Db4o.CS/native/Db4objects.Db4o.CS.Internal/ServerPlatform.cs
@@ -1,10 +1,10 @@
 /* Copyright (C) 2009  Versant Inc.  http://www.db4o.com */
 
-namespace Db4objects.Db4o.Internal.CS
+namespace Db4objects.Db4o.CS.Internal
 {
 	class ServerPlatform
 	{
-		internal static void TriggerMessageEvent(System.EventHandler<MessageEventArgs> eventHandler, Db4objects.Db4o.Internal.CS.Messages.Msg message)
+		internal static void TriggerMessageEvent(System.EventHandler<MessageEventArgs> eventHandler, Db4objects.Db4o.CS.Internal.Messages.Msg message)
 		{
 			if (eventHandler == null)
 				return;
