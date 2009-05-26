86082649f74ef8fecb3af38aa9ad71824708cdcb
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-26 17:01:11 +0000

    Use Type.AssemblyQualifiedName since in Silverlight Assembly.GetName() is not accessible.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13197 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Db4objects.Db4o/Foundation/Environments.cs
+++ Db4objects.Db4o/native/Db4objects.Db4o/Foundation/Environments.cs
@@ -1,13 +1,27 @@
-﻿namespace Db4objects.Db4o.Foundation
+﻿/* Copyright (C) 2009 Versant Inc.  http://www.db4o.com */
+using System;
+
+namespace Db4objects.Db4o.Foundation
 {
 	public partial class Environments
 	{
-		public static string DefaultImplementationFor(System.Type type)
+		public static string DefaultImplementationFor(Type type)
 		{
 			string ns = type.Namespace;
 			int lastDot = ns.LastIndexOf('.');
 			string typeName = ns.Substring(0, lastDot) + ".Internal." + ns.Substring(lastDot + 1) + "." + type.Name.Substring(1) + "Impl";
-			return typeName + ", " + type.Assembly.GetName().Name;
+			return typeName + ", " + AssemblyNameFor(type);
+		}
+
+		private static string AssemblyNameFor(Type type)
+		{
+#if SILVERLIGHT
+			string fullyQualifiedTypeName = type.AssemblyQualifiedName;
+			int assemblyNameSeparator = fullyQualifiedTypeName.IndexOf(',');
+			return fullyQualifiedTypeName.Substring(assemblyNameSeparator + 1);
+#else
+			return type.Assembly.GetName().Name;
+#endif
 		}
 	}
 }
