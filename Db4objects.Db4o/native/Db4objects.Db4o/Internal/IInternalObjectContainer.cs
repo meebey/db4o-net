df66ee2e00d348f209618d4d3eca2ca542907171
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-08-26 15:02:27 +0000

    non generic System.Action is not available on .net < 3.5
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13576 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/Db4objects.Db4o/Internal/IInternalObjectContainer.cs
+++ Db4objects.Db4o/native/Db4objects.Db4o/Internal/IInternalObjectContainer.cs
@@ -1,10 +1,16 @@
 ﻿/* Copyright (C) 2009  Versant Inc.  http://www.db4o.com */
-using System;
+#if NET_2_0 || CF_2_0
+namespace System
+{
+	public delegate void Action();
+}
+#endif
 
 namespace Db4objects.Db4o.Internal
 {
+
 	public partial interface IInternalObjectContainer
 	{
-		void WithEnvironment(Action action);
+		void WithEnvironment(System.Action action);
 	}
 }
