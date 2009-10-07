dd7fdc4f4e62452a476d3592cb8558467db909cf
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-10-07 12:19:20 +0000

    acv+pr: COR-1749: Tests cases for performance counters
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13775 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Monitoring/AllTests.cs
@@ -0,0 +1,25 @@
+﻿/* Copyright (C) 2009   Versant Inc.   http://www.db4o.com */
+
+#if !CF && !SILVERLIGHT
+using System;
+using Db4oUnit.Extensions;
+
+namespace Db4objects.Db4o.Tests.CLI1.Monitoring
+{
+	class AllTests : Db4oTestSuite
+	{
+		protected override Type[] TestCases()
+		{
+			return new Type[]
+			       	{
+						typeof(ClientConnectionsPerformanceCounterTestCase),
+						typeof(ClientNetworkingPerformanceCounterTestCase),
+						typeof(NativeQueryMonitoringSupportTestCase),
+						typeof(PerObjectContainerPerformanceCounterTestCase),
+			       		typeof(QueryMonitoringSupportTestCase),
+						typeof(ServerNetworkingPerformanceCounterTestCase),
+					};
+		}
+	}
+}
+#endif
---
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Monitoring/ClientConnectionsPerformanceCounterTestCase.cs
@@ -0,0 +1,95 @@
+﻿/* Copyright (C) 2009   Versant Inc.   http://www.db4o.com */
+#if !CF && !SILVERLIGHT
+
+using System.Diagnostics;
+using System.Threading;
+using Db4objects.Db4o.CS;
+using Db4objects.Db4o.CS.Internal;
+using Db4objects.Db4o.Events;
+using Db4objects.Db4o.Ext;
+using Db4objects.Db4o.Internal;
+using Db4objects.Db4o.Monitoring.Internal;
+using Db4objects.Db4o.Tests.Common.Api;
+using Db4oUnit;
+
+namespace Db4objects.Db4o.Tests.CLI1.Monitoring
+{
+	public class ClientConnectionsPerformanceCounterTestCase : TestWithTempFile
+	{
+		private const string UserName = "db4o";
+		private const string Password = "db4o";
+
+		public void TestConnectedClients() 
+		{
+			for(int i=0; i < 500; i++) 
+			{
+				Assert.AreEqual(0, ConnectedClientCount(), "No client yet.");
+				IExtObjectContainer client1 = OpenNewSession();
+				Assert.AreEqual(1, ConnectedClientCount(), "client1:" + i);
+				IExtObjectContainer client2 = OpenNewSession();
+				Assert.AreEqual(2, ConnectedClientCount(), "client1 and client2: " + i);
+				EnsureClose(client1);
+				Assert.AreEqual(1, ConnectedClientCount(), "client2: " + i);
+				EnsureClose(client2);
+				Assert.AreEqual(0, ConnectedClientCount(), "" + i);
+			}		
+		}
+
+		private void EnsureClose(IExtObjectContainer client) 
+		{
+			client.Close();
+			_clientDisconnectedEvent.WaitOne();
+		}
+
+		private IExtObjectContainer OpenNewSession() 
+		{
+			return (IExtObjectContainer) Db4oClientServer.OpenClient("localhost", _server.Ext().Port(), UserName, Password);
+		}
+
+		private long ConnectedClientCount() 
+		{
+			return Db4oPerformanceCounterCategory.CounterForNetworkingClientConnections(_server.Ext().ObjectContainer()).RawValue;
+		}
+
+		public override void SetUp()
+		{
+			Db4oPerformanceCounterInstaller.ReInstall();
+			//_server = Db4oClientServer.OpenServer(TempFile(), Db4oClientServer.ArbitraryPort);
+			_server = Db4oClientServer.OpenServer(TempFile(), -1);
+			_server.GrantAccess(UserName, Password);
+
+			ObjectContainerBase container = (ObjectContainerBase) _server.Ext().ObjectContainer();
+			container.WithEnvironment(delegate
+			{
+				_clientConnections = Db4oPerformanceCounterCategory.CounterForNetworkingClientConnections(_server);
+			});
+
+			RegisterForClientDisconnectionEvents((IObjectServerEvents)_server);
+		}
+
+		public override void TearDown()
+		{
+			_clientConnections.Dispose();
+			_clientDisconnectedEvent.Close();
+			_server.Close();
+
+			base.TearDown();
+		}
+
+		private void RegisterForClientDisconnectionEvents(IObjectServerEvents serverEvents)
+		{
+			serverEvents.ClientDisconnected += ClientDisconnected;
+		}
+
+		void ClientDisconnected(object sender, StringEventArgs e)
+		{
+			_clientDisconnectedEvent.Set();
+		}
+
+		private IObjectServer _server;
+		private PerformanceCounter _clientConnections;
+		private readonly EventWaitHandle _clientDisconnectedEvent = new EventWaitHandle(false, EventResetMode.AutoReset);
+	}
+}
+
+#endif
