79b014c4a0cd2dc750c89075bb7ed4c96244916a
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-10-13 13:27:42 +0000

    removed unused field
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13806 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Monitoring/PerformanceCounterTestCaseBase.cs
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/CLI1/Monitoring/PerformanceCounterTestCaseBase.cs
@@ -9,8 +9,6 @@ namespace Db4objects.Db4o.Tests.CLI1.Monitoring
 {
 	public class PerformanceCounterTestCaseBase : AbstractDb4oTestCase
 	{
-		private static bool _installed;
-
 		protected override void Db4oSetupBeforeConfigure()
 		{
 			Db4oPerformanceCounterInstaller.ReInstall();
