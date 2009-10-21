8b8377f0a07182a33989e1d460745019b9b6bfec
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-10-21 13:03:33 +0000

    acv+pr: INT-112 : SerializableAttribute was introduced in CF SP1 (http://blogs.msdn.com/netcfteam/archive/2006/04/21/580901.aspx)
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13902 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/native/compact/Compat.cs
+++ Db4objects.Db4o/native/compact/Compat.cs
@@ -1,12 +1,5 @@
 /* Copyright (C) 2004   Versant Inc.   http://www.db4o.com */
 #if CF_2_0
-namespace System
-{
-	class SerializableAttribute : Attribute
-	{
-	}
-}
-
 namespace Db4objects.Db4o
 {
 	/// <exclude />
