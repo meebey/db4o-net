a93a4aa204440dad127c6e1fecd6867332a27042
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-11-06 01:05:20 +0000

    COR-1817 - server configuration item tests
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@13980 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/Common/CS/Config/MockServer.cs
@@ -0,0 +1,11 @@
+#if !SILVERLIGHT
+using System;
+
+namespace Db4objects.Db4o.Tests.Common.CS.Config
+{
+	public partial class MockServer : IDisposable {
+		public void Dispose() {
+		}
+	}
+}
+#endif // !SILVERLIGHT
