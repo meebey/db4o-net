831a074a3d603fba04626b590328deff28a67c29
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-11-27 00:01:39 +0000

    acv: fixed a bug in the alias. It was failing cross platform tests
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@14055 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Optional/native/Db4objects.Db4o/Config/JavaSupport.cs
+++ Db4objects.Db4o.Optional/native/Db4objects.Db4o/Config/JavaSupport.cs
@@ -37,7 +37,7 @@ namespace Db4objects.Db4o.Config
 		{
 #if !SILVERLIGHT
 			config.AddAlias(new TypeAlias("com.db4o.User", FullyQualifiedName(typeof(User))));
-			config.AddAlias(new TypeAlias("com.db4o.cs.internal.messages.MUserMessage$UserMessagePayload", FullyQualifiedName(typeof(Db4objects.Db4o.CS.Internal.Messages.MUserMessage.UserMessagePayload))));
+			config.AddAlias(new TypeAlias("com.db4o.cs.internal.messages.MUserMessage$UserMessagePayload", "Db4objects.Db4o.CS.Internal.Messages.MUserMessage+UserMessagePayload, Db4objects.Db4o.CS"));
 			config.AddAlias(new WildcardAlias("com.db4o.cs.internal.*", "Db4objects.Db4o.CS.Internal.*, Db4objects.Db4o.CS"));
 #endif
 		}
