d2d136f50c78fa525641bb94b6169f79fc632cb8
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-12-15 12:13:56 +0000

    Copyright notice
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@14125 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/Common/Migration/AllTests.cs
+++ Db4objects.Db4o.Tests/native/Db4objects.Db4o.Tests/Common/Migration/AllTests.cs
@@ -1,6 +1,5 @@
+/* Copyright (C) 2009 Versant Inc.   http://www.db4o.com */
 using System;
-using System.Text;
-using Db4objects.Db4o.Tests.Common.Migration;
 using Db4oUnit.Extensions;
 
 namespace Db4objects.Db4o.Tests.Common.Migration
