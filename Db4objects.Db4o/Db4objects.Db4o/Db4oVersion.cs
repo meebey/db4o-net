261fbb9ce72b686daaa0bd8a830194c4db13d5a8
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2010-04-21 20:08:02 +0000

    db4o.net update
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@14360 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o/Db4objects.Db4o/Db4oVersion.cs
+++ Db4objects.Db4o/Db4objects.Db4o/Db4oVersion.cs
@@ -5,13 +5,13 @@ namespace Db4objects.Db4o
 	/// <exclude></exclude>
 	public class Db4oVersion
 	{
-		public static readonly string Name = "7.13.143.14312";
+		public static readonly string Name = "7.13.144.14312";
 
 		public const int Major = 7;
 
 		public const int Minor = 13;
 
-		public const int Iteration = 143;
+		public const int Iteration = 144;
 
 		public const int Revision = 14312;
 	}
