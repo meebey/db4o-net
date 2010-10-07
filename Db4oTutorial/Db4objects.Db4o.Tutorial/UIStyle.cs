54dc3c06dc91652e017c99cfb8103eb7b80f659c
roman_stoffel <roman_stoffel@915b37e7-0715-0410-8bc3-b14b59444072>
2010-10-07 14:09:12 +0000

    rs:DOC-473 Change tutorial look & feel to Versant: Changed colors for .NET to Versant-style
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@14962 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4oTutorial/Db4objects.Db4o.Tutorial/UIStyle.cs
+++ Db4oTutorial/Db4objects.Db4o.Tutorial/UIStyle.cs
@@ -10,9 +10,9 @@ namespace Db4objects.Db4o.Tutorial
 	/// </summary>
 	public class UIStyle
 	{
-		public static readonly Color Db4oGrey = Color.FromArgb(0xFF, 0x66, 0x61, 0x77);
-		
-		public static readonly Color Db4oGreen = Color.FromArgb(0xFF, 0xAD, 0xD6, 0x5C);
+        public static readonly Color Db4oGrey = Color.FromArgb(0xFF, 255, 255, 255);
+
+        public static readonly Color Db4oRed = Color.FromArgb(0xFF, 187, 30, 30);
     
 		public static readonly Color BackColor = Color.FromArgb(0xFF, 0x83, 0x83, 0x83);
 		
@@ -27,13 +27,13 @@ namespace Db4objects.Db4o.Tutorial
 		public static void ApplyConsoleStyle(Control control)
 		{
 			control.BackColor = UIStyle.BackColor;
-			control.ForeColor = UIStyle.Db4oGreen;
+            control.ForeColor = Color.Black;
 		}
 		
 		public static void ApplyButtonStyle(Control control)
 		{
 			control.ForeColor = UIStyle.Db4oGrey;
-			control.BackColor = UIStyle.Db4oGreen;
+			control.BackColor = UIStyle.Db4oRed;
 		}
 		
 		private UIStyle()
