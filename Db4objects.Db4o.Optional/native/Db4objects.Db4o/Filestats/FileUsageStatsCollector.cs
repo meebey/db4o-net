2eef86df8cdadf7fe4fa87bcd2b7c14ed1d7caa9
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-12-01 17:22:08 +0000

    cr + acv: FileUsageStatsCollector now works nicely, also for .NET.
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@15264 915b37e7-0715-0410-8bc3-b14b59444072

---
+++ Db4objects.Db4o.Optional/native/Db4objects.Db4o/Filestats/FileUsageStatsCollector.cs
@@ -0,0 +1,17 @@
+/* Copyright (C) 2004 - 2010  Versant Inc.  http://www.db4o.com */
+
+using Db4objects.Db4o.Internal.Collections;
+
+namespace Db4objects.Db4o.Filestats
+{
+
+    public partial class FileUsageStatsCollector
+    {
+
+        private void RegisterBigSetCollector()
+        {
+            MiscCollectors[typeof(BigSet<>).FullName] = new BigSetMiscCollector();
+        }
+
+    }
+}
