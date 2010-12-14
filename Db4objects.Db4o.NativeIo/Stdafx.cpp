0680d033c65f59be0cd746642d88c6af654e19bd
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2010-12-14 14:41:47 +0000

    acv: db4o do not rely on native code. Delete unused project
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@15295 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.NativeIo/Stdafx.cpp
+++
@@ -1,3 +0,0 @@
-/* Copyright (C) 2006   db4objects Inc.   http://www.db4o.com */
-
-#include "Stdafx.h"
--- Db4objects.Db4o.NativeIo/Stdafx.h
+++
@@ -1,5 +0,0 @@
-/* Copyright (C) 2006   db4objects Inc.   http://www.db4o.com */
-
-#pragma once
-
-#include <windows.h>
