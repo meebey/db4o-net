6f3c5d8ad54140212731b58356fe12452b069e6b
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2011-01-13 20:33:47 +0000

    acv: Allow test to run on SL
    git-svn-id: http://source.db4o.com/db4o/trunk/db4o.net@15362 915b37e7-0715-0410-8bc3-b14b59444072

--- Db4objects.Db4o.Tests/native/Silverlight/Db4objects.Db4o.Tests/Common/Api/Db4oTestWithTempFile.cs
+++ Db4objects.Db4o.Tests/native/Silverlight/Db4objects.Db4o.Tests/Common/Api/Db4oTestWithTempFile.cs
@@ -1,16 +1,15 @@
 ﻿/* Copyright (C) 2009  Versant Inc.   http://www.db4o.com */
 #if SILVERLIGHT
 using Db4objects.Db4o.Config;
-using Db4objects.Db4o.IO;
 
 namespace Db4objects.Db4o.Tests.Common.Api
 {
 	public class Db4oTestWithTempFile : TestWithTempFile
 	{
-		protected IEmbeddedConfiguration NewConfiguration()
+		protected virtual IEmbeddedConfiguration NewConfiguration()
 		{
 			IEmbeddedConfiguration config = Db4oEmbedded.NewConfiguration();
-			config.File.Storage = new IsolatedStorageStorage();
+			config.AddConfigurationItem(new SilverlightSupport());
 			return config;
 		}
 	}
